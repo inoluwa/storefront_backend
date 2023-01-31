da4ff979207fc9c19724ebbf6d7e2d54f5415026
unknown <naomiadeniran426@gmail.com>
2023-01-31 13:07:01 +0100

    modify database testing

--- src/database.ts
+++ src/database.ts
@@ -8,15 +8,30 @@ const {
     POSTGRES_DB,
     POSTGRES_USER,
     POSTGRES_PASSWORD,
-    POSTGRES_PORT
+    POSTGRES_PORT,
+    TEST_POSTGRES_DB,
+    TEST_POSTGRES_USER,
+    ENV_CHECKER
 } = process.env
+let db;
+if(ENV_CHECKER==='dev'){
+    db = new Pool({
+        host:POSTGRES_HOST,
+        database:POSTGRES_DB,
+        user:POSTGRES_USER,
+        password:POSTGRES_PASSWORD,
+        port:Number(POSTGRES_PORT)
+    })
+}
+else if(ENV_CHECKER==='test'){
+    db = new Pool({
+        host:POSTGRES_HOST,
+        database:TEST_POSTGRES_DB,
+        user:TEST_POSTGRES_USER,
+        password:POSTGRES_PASSWORD,
+        port:Number(POSTGRES_PORT)
+    })
+}
 
-const db = new Pool({
-    host:POSTGRES_HOST,
-    database:POSTGRES_DB,
-    user:POSTGRES_USER,
-    password:POSTGRES_PASSWORD,
-    port:Number(POSTGRES_PORT)
-})
 
 export default db
